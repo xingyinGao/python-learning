a2fca6a22466ad6621525ba6a3cad8f5ad1d0034
xingyinGao <43268719+xingyinGao@users.noreply.github.com>
2019-01-01 16:41:22 +0800

    load_data_python.py

---
+++ load_data_python.py
@@ -0,0 +1,27 @@
+from sklearn import datasets
+
+def load_data_python(key='iris'):
+    """
+    #python自带数据集加载：
+    #      iris：鸢尾花数据集；
+    #      breat_cancer:乳腺癌数据集；
+    #      digits：手写数字数据集；
+    #      diabetes：糖尿病数据集；
+    #      boston：波士顿房价数据集；
+    #      linnerud：体能数据集
+    """
+    if key=='iris':
+        return datasets.load_iris()
+    elif key=='breat_cancer':
+        return datasets.load_breat_cancer()
+    elif key=='digits':
+        return datasets.load_digits()
+    elif key=='diabetes':
+        return datasets.load_diabetes()
+    elif key=='boston':
+        return datasets.load_boston()
+    elif key=='linnerud':
+           return datasets.load_linnerud() 
+    else:
+          print(key+'数据集不存在！请检查参数') 
+    return  
